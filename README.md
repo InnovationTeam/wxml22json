eebf1460938ce3bc4cb1e26410d230498de5c810
Kali-Hac <huaixiaocong@qq.com>
2018-05-15 16:53:23 +0800

    新版本更新

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ## 使用方法（demo.js）
 ```
 //导入wxml和标准json互转的库
-var wxml2json = require("./wxml2json").wxml2json;
-var json2wxml = require('./wxml2json').json2wxml;
+var wxml2json = require("wxml2json").wxml2json;
+var json2wxml = require('wxml2json').json2wxml;
 var nanoid = require('nanoid');
+var methods = require('./methods');
 
 // 测试用的wxml代码
 var wxml = "<view class='section'>\
@@ -17,15 +18,35 @@ var wxml = "<view class='section'>\
 
 //标准json
 var origin_json = wxml2json(wxml);
-console.log(JSON.stringify(origin_json,null,4));
+// console.log(JSON.stringify(origin_json,null,4));
 
 //新格式的json
-var new_json = generate_json(origin_json);
-console.log(JSON.stringify(new_json,null,4));
+var new_json = methods.generate_json(origin_json);
+// console.log(JSON.stringify(new_json,null,4));
 
 //恢复成标准的json
-var rec_json = recover_json(new_json);
-console.log(JSON.stringify(rec_json,null,4));
+var rec_json = methods.recover_json(new_json);
+// console.log(JSON.stringify(rec_json,null,4));
+
+//恢复为wxml代码
+var rec_wxml = json2wxml(rec_json);
+console.log(rec_wxml);
+
+// 将id为f_id的组件移动到id为t_id的组件下
+var f_id = new_json["children"][2];
+var t_id = new_json["children"][4];
+console.log("f_id " + f_id + '\n' + "t_id " + t_id + '\n');
+//拖拽某个组件，先执行移除id操作
+methods.remove(f_id, new_json);
+// console.log(JSON.stringify(new_json,null,4));
+//再执行插入id操作
+methods.insert(f_id, t_id, new_json);
+//输出结果
+// console.log(JSON.stringify(new_json,null,4));
+
+//恢复成标准的json
+var rec_json = methods.recover_json(new_json);
+// console.log(JSON.stringify(rec_json,null,4));
 
 //恢复为wxml代码
 var rec_wxml = json2wxml(rec_json);
@@ -35,17 +56,17 @@ console.log(rec_wxml);
 
 ## 接口API（methods.js）
 ```
-// 输入组件id和新格式的json，从父组件children移除组件id及子子组件id
-function remove(id, json)
+// 输入组件id和新格式的json，从父组件children移除组件id
+module.exports.remove = function remove(id, json)
 
-//输入组件id，它的子组件id，info数组，将子子组件id从父组件children移除
-function findParent_json_and_remove(id, children_id, father_id, info)
+//输入组件id，info数组，将子组件id从父组件children移除
+function findParent_json_and_remove(id, father_id, info)
 
-//输入组件id以及它的父组件id，将组件和它的子子组件id插入到父组件children
-function insert(id, father_id, json)
+//输入组件id以及它的父组件id，将组件id插入到父组件children
+module.exports.insert = function insert(id, father_id, json)
 
-//输入组件id和它的子组件id、父组件id，info数组，将子子组件id加入父组件children
-function findParent_json_and_insert(id,children_id,father_id, info)
+//输入组件id、父组件id，info数组，将子子组件id加入父组件children
+function findParent_json_and_insert(id,father_id, info)
 
 //输入原始标准json，输出新格式json
 function generate_json(origin_json)
@@ -57,6 +78,9 @@ function identify_json(id_json, father_id, child_id, data)
 function recover_json(new_json)
 
 //为上者生成过程中所调用方法，输入为新格式json，寻找子组件的id，
-//还有已经添加的子组件child_maps，防止重复添加(由于父组件存的不是直接子组件的id)
+//还有已经添加的子组件child_maps，防止重复添加(由于根存的不是所有子组件的id)
 function findChild_json_and_insert(new_json, id, child_maps)
 ```
+
+## 另一个版本的实现为children里放的是直接子组件和非直接子组件
+### 递归处理情况比较复杂，有参考价值，详情可见another_version
--- another_version/methods.js
+++ another_version/methods.js
@@ -1,33 +1,57 @@
 var nanoid = require('nanoid');
-// 输入组件id和新格式的json，从父组件children移除组件id
+// 输入组件id和新格式的json，从父组件children移除组件id及子子组件id
 module.exports.remove = function remove(id, json){
-    // let children_id = json["info"][id].children;
+    let children_id = json["info"][id].children;
     let father_id = json["info"][id].fatherID;
-    findParent_json_and_remove(id, father_id, json["info"]);
+    findParent_json_and_remove(id, children_id, father_id, json["info"]);
 }
 
-//输入组件id，info数组，将子组件id从父组件children移除
-function findParent_json_and_remove(id, father_id, info){
+//输入组件id，它的子组件id，info数组，将子子组件id从父组件children移除
+function findParent_json_and_remove(id, children_id, father_id, info){
     if(father_id === 'root')
         return;
-    let index = info[father_id].children.indexOf(id);
-    if(index!==1)
-        info[father_id].children.splice(index, 1);
+    let temp_ids = new Array();
+    console.log(father_id);
+    for(let i=0;i<info[father_id].children.length;i++){
+        var flag =new Boolean();
+        flag=false;
+        if(info[father_id].children[i] !== id){
+            flag=false;
+            for(let k=0;k<children_id.length;k++){
+                if(info[father_id].children[i] === children_id[k]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag === false)
+                temp_ids.push(info[father_id].children[i]);
+        }
+    }
+    info[father_id].children = temp_ids;
+    findParent_json_and_remove(id, children_id, info[father_id].fatherID, info);
 }
 
-//输入组件id以及它的父组件id，将组件id插入到父组件children
+//输入组件id以及它的父组件id，将组件和它的子子组件id插入到父组件children
 module.exports.insert = function insert(id, father_id, json){
-    // let children_id = json["info"][id].children;
+    let children_id = json["info"][id].children;
     json["info"][id].fatherID = father_id;
-    findParent_json_and_insert(id,father_id,json["info"]);
+    findParent_json_and_insert(id,children_id,father_id,json["info"]);
 }
 
-//输入组件id、父组件id，info数组，将子子组件id加入父组件children
-function findParent_json_and_insert(id,father_id, info){
+//输入组件id和它的子组件id、父组件id，info数组，将子子组件id加入父组件children
+function findParent_json_and_insert(id,children_id,father_id, info){
     if(father_id === 'root')
         return;
-    if(info[father_id].children.indexOf(id) === -1)
+    if(info[father_id].children.indexOf(id) === -1){
         info[father_id].children.push(id);
+        console.log(children_id);
+    for(let j=0;j<children_id.length;j++){
+        if(info[father_id].children.indexOf(children_id[j]) === -1)
+            info[father_id].children.push(children_id[j]);
+        }
+    }
+    if(father_id !== 'root')
+        findParent_json_and_insert(id,children_id,info[father_id].fatherID,info);
 }
 
 //输入原始标准json，输出新格式json
@@ -44,7 +68,8 @@ module.exports.generate_json = function generate_json(origin_json){
   for(var i=0; i<origin_json.children.length; i++) 
   { 
     let t = identify_json(id_json, root_id, id_json["children"][i], origin_json.children[i]);
-    id_json["info"][t["id"]] = t; 
+    id_json["children"] = id_json["children"].concat(t[0]);
+    id_json["info"][t[1].id] = t[1]; 
   }  
   return id_json;
 }
@@ -82,11 +107,14 @@ function identify_json(id_json, father_id, child_id, data){
     for(var i=0; i<data.children.length; i++) 
     {   
         let t =  identify_json(id_json, temp["id"], temp["children"][i], data.children[i]);
-        id_json["children"] = id_json["children"].concat(t["id"]);
-        id_json["info"][t["id"]] = t; 
+        temp["children"] = temp["children"].concat(t[0]);
+        id_json["info"][t[1].id] = t[1];
     }  
  }
-  return temp;
+  var ids_temp = new Array();
+  ids_temp.push(temp["children"]);
+  ids_temp.push(temp);
+  return ids_temp;
 }
 
 //输入新格式json，输出标准格式json
@@ -107,7 +135,7 @@ module.exports.recover_json = function recover_json(new_json){
 }
 
 //为上者生成过程中所调用方法，输入为新格式json，寻找子组件的id，
-//还有已经添加的子组件child_maps，防止重复添加(由于根存的不是所有子组件的id)
+//还有已经添加的子组件child_maps，防止重复添加(由于父组件存的不是直接子组件的id)
 function findChild_json_and_insert(new_json, id, child_maps){
     if(child_maps[id] ===0)
         child_maps[id] = 1;
